c779ac724a7ba8caf796c76d3b67fd0a1b8a642c
Nick <grifon52@gmail.com>
2020-02-04 22:49:46 +0300

    MariaDB specs

--- schemakeeper-server/src/test/scala/schemakeeper/server/service/MariaDBStorageTest.scala
+++ schemakeeper-server/src/test/scala/schemakeeper/server/service/MariaDBStorageTest.scala
@@ -4,7 +4,7 @@ import java.sql.{Connection, DriverManager}
 import java.util
 
 import cats.Id
-import com.dimafeng.testcontainers.MariaDBContainer
+import com.dimafeng.testcontainers.{ContainerDef, JdbcDatabaseContainer, MariaDBContainer, SingleContainer}
 import com.dimafeng.testcontainers.scalatest.TestContainerForAll
 import com.typesafe.config.{Config, ConfigFactory}
 import org.junit.runner.RunWith
@@ -14,12 +14,13 @@ import schemakeeper.server.Configuration
 
 @RunWith(classOf[JUnitRunner])
 class MariaDBStorageTest extends ServiceTest with TestContainerForAll with BeforeAndAfterEach {
-  override val containerDef: MariaDBContainer.Def =
-    MariaDBContainer.Def(dockerImageName = "mariadb:10.3.6", dbName = "schemakeeper")
+  override val containerDef: MariaDBStorageTest.MariaDBContainer.Def =
+    MariaDBStorageTest.MariaDBContainer.Def(dbName = "schemakeeper")
+
   override var schemaStorage: DBBackedService[Id] = _
   var connection: Connection = _
 
-  override def afterContainersStart(container: MariaDBContainer): Unit = {
+  override def afterContainersStart(container: MariaDBStorageTest.MariaDBContainer): Unit = {
     val map: util.Map[String, AnyRef] = new util.HashMap[String, AnyRef]
     map.put("schemakeeper.storage.username", container.username)
     map.put("schemakeeper.storage.password", container.password)
@@ -42,5 +43,61 @@ class MariaDBStorageTest extends ServiceTest with TestContainerForAll with Befor
     connection.commit()
   }
 
-  override def beforeContainersStop(containers: MariaDBContainer): Unit = connection.close()
+  override def beforeContainersStop(containers: MariaDBStorageTest.MariaDBContainer): Unit = connection.close()
+}
+
+object MariaDBStorageTest {
+  import org.testcontainers.containers.{MariaDBContainer => JavaMariaDBContainer}
+
+  case class MariaDBContainer(
+    dockerImageName: String = MariaDBContainer.defaultDockerImageName,
+    dbName: String = MariaDBContainer.defaultDatabaseName,
+    dbUsername: String = MariaDBContainer.defaultUsername,
+    dbPassword: String = MariaDBContainer.defaultPassword,
+    configurationOverride: Option[String] = None
+  ) extends SingleContainer[JavaMariaDBContainer[_]]
+      with JdbcDatabaseContainer {
+
+    override val container: JavaMariaDBContainer[_] = {
+      val c = new JavaMariaDBContainer(dockerImageName)
+      c.withDatabaseName(dbName)
+      c.withUsername(dbUsername)
+      c.withPassword(dbPassword)
+      configurationOverride.foreach(c.withConfigurationOverride)
+      c
+    }
+
+    def testQueryString: String = container.getTestQueryString
+
+  }
+
+  object MariaDBContainer {
+
+    val defaultDockerImageName = s"${JavaMariaDBContainer.IMAGE}:${JavaMariaDBContainer.DEFAULT_TAG}"
+    val defaultDatabaseName = "test"
+    val defaultUsername = "test"
+    val defaultPassword = "test"
+
+    case class Def(
+      dockerImageName: String = MariaDBContainer.defaultDockerImageName,
+      dbName: String = MariaDBContainer.defaultDatabaseName,
+      dbUsername: String = MariaDBContainer.defaultUsername,
+      dbPassword: String = MariaDBContainer.defaultPassword,
+      configurationOverride: Option[String] = None
+    ) extends ContainerDef {
+
+      override type Container = MariaDBContainer
+
+      override def createContainer(): MariaDBContainer =
+        new MariaDBContainer(
+          dockerImageName,
+          dbName,
+          dbUsername,
+          dbPassword,
+          configurationOverride
+        )
+    }
+
+  }
+
 }
