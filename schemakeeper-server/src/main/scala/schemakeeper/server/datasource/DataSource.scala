a84dec55ab19e34ad1043a75a59fb1a870cccf2a
Nikita Ryanov <ryanov.nikita@gmail.com>
2020-02-09 17:26:56 +0300

    Update DataSource.scala

--- schemakeeper-server/src/main/scala/schemakeeper/server/datasource/DataSource.scala
+++ schemakeeper-server/src/main/scala/schemakeeper/server/datasource/DataSource.scala
@@ -1,44 +1,21 @@
 package schemakeeper.server.datasource
 
-import cats.effect.{IO, Resource}
+import cats.arrow.FunctionK
+import cats.effect.{Async, Blocker, ContextShift, Resource}
 import com.zaxxer.hikari.HikariConfig
 import doobie.ExecutionContexts
 import doobie.hikari.HikariTransactor
 import doobie.quill.{DoobieContext, DoobieContextBase}
 import doobie.util.transactor.Transactor
+import doobie.implicits._
 import io.getquill.{NamingStrategy, SnakeCase}
 import io.getquill.context.sql.idiom.SqlIdiom
-import schemakeeper.server.Configuration
+import schemakeeper.server.{Configuration, Storage}
 import schemakeeper.server.datasource.migration.SupportedDatabaseProvider
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 object DataSource {
-  def build(config: Configuration): Resource[IO, Transactor[IO]] = {
-    implicit val cs = IO.contextShift(global)
-
-    val cfg = new HikariConfig()
-    cfg.setMaximumPoolSize(config.databaseMaxConnections)
-    cfg.setDriverClassName(config.databaseDriver)
-    cfg.setJdbcUrl(config.databaseConnectionString)
-    cfg.setUsername(config.databaseUsername)
-    cfg.setPassword(config.databasePassword)
-    cfg.setSchema(config.databaseSchema)
-    cfg.setMinimumIdle(1)
-
-    for {
-      connectionExecutionPool <- ExecutionContexts.fixedThreadPool[IO](Runtime.getRuntime.availableProcessors())
-      transactionExecutionPool <- ExecutionContexts.cachedThreadPool[IO]
-      xa <- HikariTransactor.fromHikariConfig[IO](
-        cfg,
-        connectionExecutionPool,
-        transactionExecutionPool
-      )
-    } yield xa
-  }
-
-  def context(configuration: Configuration): DoobieContextBase[_ <: SqlIdiom, _ <: NamingStrategy] =
-    DataSourceUtils.detectDatabaseProvider(configuration.databaseConnectionString) match {
+  def context(cfg: Configuration): DoobieContextBase[_ <: SqlIdiom, _ <: NamingStrategy] =
+    DataSourceUtils.detectDatabaseProvider(cfg.storage.url) match {
       case SupportedDatabaseProvider.PostgreSQL => new DoobieContext.Postgres(SnakeCase)
       case SupportedDatabaseProvider.MySQL      => new DoobieContext.MySQL(SnakeCase)
       case SupportedDatabaseProvider.H2         => new DoobieContext.H2(SnakeCase)
@@ -46,4 +23,39 @@ object DataSource {
       case SupportedDatabaseProvider.MariaDB => new DoobieContext.MySQL(SnakeCase)
       case SupportedDatabaseProvider.Oracle  => new DoobieContext.Oracle(SnakeCase)
     }
+
+  def resource[F[_]: Async: ContextShift](config: Configuration): Resource[F, FunctionK[doobie.ConnectionIO, F]] =
+    transactor[F](config).map { tx =>
+      def transact[A](tx: Transactor[F])(sql: doobie.ConnectionIO[A]): F[A] =
+        sql.transact(tx)
+
+      new FunctionK[doobie.ConnectionIO, F] {
+        def apply[A](l: doobie.ConnectionIO[A]): F[A] = transact(tx)(l)
+      }
+    }
+
+  private def transactor[F[_]: Async: ContextShift](config: Configuration): Resource[F, Transactor[F]] =
+    for {
+      connectionExecutionPool <- ExecutionContexts.fixedThreadPool[F](Runtime.getRuntime.availableProcessors())
+      transactionExecutionPool <- Blocker[F]
+      cfg = hikariConfig(config.storage)
+      xa <- HikariTransactor.fromHikariConfig[F](
+        cfg,
+        connectionExecutionPool,
+        transactionExecutionPool
+      )
+    } yield xa
+
+  private def hikariConfig(storage: Storage): HikariConfig = {
+    val cfg = new HikariConfig()
+    cfg.setMaximumPoolSize(storage.maxConnections)
+    cfg.setDriverClassName(storage.driver)
+    cfg.setJdbcUrl(storage.url)
+    cfg.setUsername(storage.username)
+    cfg.setPassword(storage.password.orNull)
+    cfg.setSchema(storage.schema)
+    cfg.setMinimumIdle(1)
+
+    cfg
+  }
 }
